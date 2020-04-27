23f903ebeca0164c0167e4ecb1c4aec924d005ec
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-04-27 15:21:04 +0900

    p136-137練習3-5

---
+++ ch03/Exercise5.java
@@ -0,0 +1,26 @@
+package ch03;
+
+/**
+ * p135 練習3-5
+ * switch文を用いて条件を満たすプログラムを作成
+ */
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更 >");
+        int selected = new java.util.Scanner(System.in).nextInt();
+        switch (selected){
+            case 1:
+                System.out.print("検索します");
+                break;
+            case 2:
+                System.out.print("登録します");
+                break;
+            case 3:
+                System.out.print("削除します");
+                break;
+            case 4:
+                System.out.print("変更します");
+                break;
+        }
+    }
+}
