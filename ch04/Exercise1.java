716b7b5a3891e9b16a706039d97916ecd574dead
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-04-28 15:34:43 +0900

    p176 練習4-1

---
+++ ch04/Exercise1.java
@@ -0,0 +1,19 @@
+package ch04;
+
+/**
+ * p176 練習4-1
+ * 次の条件を満たす各配列を準備するプログラムを作成せよ。
+ * ただし以下の4つの配列を1つのプログラム中に記述してよい。値の初期化も不要。
+ * 1. int型の値を4個まとめて格納できる配列points
+ * 2. double型の値を5個まとめて格納できる配列weights
+ * 3. boolean型の値を3つまとめて格納できる配列answers
+ * 4. String型の値を3つまとめて格納できる配列names
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        int[] points = new int[4];
+        double[] weights = new double[5];
+        boolean[] answers = new boolean[3];
+        String[] names = new String[3];
+    }
+}
