d1031cf27b72b67741a2208d61ad43144cc56a5b
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-04-30 10:11:31 +0900

    変数名を修正

--- ch04/Exercise4.java
+++ ch04/Exercise4.java
@@ -14,8 +14,8 @@ public class Exercise4 {
         int[] numbers = {3, 4, 9};
         System.out.println("1桁の数字を入力してください");
         int input = new java.util.Scanner(System.in).nextInt();
-        for (int n : numbers) {
-            if (input == n) {
+        for (int number : numbers) {
+            if (input == number) {
                 System.out.println("アタリ！");
             }
         }
