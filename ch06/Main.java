26ea8b0a1537262307be734bf61ca3ec0ca5021b
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-05-01 11:26:38 +0900

    p263 練習6-1

---
+++ ch06/Main.java
@@ -0,0 +1,20 @@
+package ch06;
+
+/**
+ * p263 練習6-1
+ * (1) commentパッケージに属するZenhanクラスにdoWarusa()とdoTogame()を定義
+ * (2) commentパッケージに属するKouhanクラスにcallDeae()とshowMondokoro()を定義
+ * (3) デフォルトパッケージに属するMainクラスにはmainメソッドだけを定義
+ *      ただしMainクラスの先頭でインポートするのはZenhanクラスのみとする
+ */
+
+import ch06.comment.Zenhan;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTogame();
+        ch06.comment.Kouhan.callDeae();
+        ch06.comment.Kouhan.showMondokoro();
+    }
+}
---
+++ ch06/comment/Zenhan.java
@@ -0,0 +1,16 @@
+package ch06.comment;
+
+/**
+ * p263 練習6-1 (1)
+ * commentパッケージに属するZenhanクラスを作成
+ * doWarusaメソッドとdotogameメソッドを定義
+ */
+public class Zenhan {
+    public static void doWarusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせませんぞ。");
+    }
+}
