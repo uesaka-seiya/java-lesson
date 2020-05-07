f8eec2af922f3240356ceb8cb8fd11f5a32adba8
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-05-07 14:46:08 +0900

    p329 練習8-2

--- ch08/Cleric.java
+++ ch08/Cleric.java
@@ -3,6 +3,15 @@ package ch08;
 /**
  * p329 練習8-1
  * 聖職者「クレリック」を表現するクラスClericをpublicで宣言。
+ * p329 練習8-2
+ * 「名前」「HP」「最大HP」「MP」「最大MP」を属性として追加。
+ * HPと最大HPの初期値は50(整数), MPと最大MPの初期値は10(整数)。
+ * 最大HPと最大MPは定数フィールドで宣言。
  */
 public class Cleric {
+    String name;
+    int hp;
+    final int MAX_HP = 50;
+    int mp;
+    final int MAX_MP = 10;
 }
