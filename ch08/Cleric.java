72990d2bcaa624f9dd8d7afba4c7a6071ff4b8af
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-05-07 14:47:29 +0900

    p329 練習8-3

--- ch08/Cleric.java
+++ ch08/Cleric.java
@@ -7,6 +7,9 @@ package ch08;
  * 「名前」「HP」「最大HP」「MP」「最大MP」を属性として追加。
  * HPと最大HPの初期値は50(整数), MPと最大MPの初期値は10(整数)。
  * 最大HPと最大MPは定数フィールドで宣言。
+ * p329 練習8-3
+ * selfAid()を追加。引数なし、戻り値なし。
+ * MPを5消費することで自分自身のHPを最大HPまで回復するメソッド。
  */
 public class Cleric {
     String name;
@@ -14,4 +17,14 @@ public class Cleric {
     final int MAX_HP = 50;
     int mp;
     final int MAX_MP = 10;
+
+    public void selfAid() {
+        System.out.println(this.name + "は、セルフエイドを唱えた！");
+        this.hp = this.MAX_HP;
+        this.mp -= 5;
+        System.out.println("HPが最大まで回復した");
+        System.out.println("MPは"+ this.mp + "になった");
+    }
+
+
 }
