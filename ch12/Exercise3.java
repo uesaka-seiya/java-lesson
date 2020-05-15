fb55b527adf4f7f7ea074c37eaddfa85af1d49b8
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-05-15 09:55:42 +0900

    chore: 配列の初期化を1行に変更した
    後学のため、教本では3行で個別に行っていた要素の確保および各要素への代入を、まとめて行う書式に変更した。

--- ch12/Exercise3.java
+++ ch12/Exercise3.java
@@ -8,9 +8,7 @@ package ch12;
  */
 public class Exercise3 {
     public static void main(String[] args) {
-        Y[] array = new Y[2];
-        array[0] = new A();
-        array[1] = new B();
+        Y[] array = { new A(), new B() };
         for (Y y : array) {
             y.b();
         }
