b2957cb065b20172da98290a23bf4885abac60a5
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-05-26 09:39:27 +0900

    add: 練習17-2のプログラムを作成した
    Exercise1.javaに追記して作成した

--- ch17/Exercise1.java
+++ ch17/Exercise1.java
@@ -2,13 +2,27 @@ package ch17;
 
 /**
  * 練習1
- * 次のようなプログラムを実行しエラーを発生させる
+ * 次のようなプログラムを実行し実行時エラーを発生させる
  * ①String型変数sを宣言しnullを宣言する
  * ②s.length()の内容を表示しようとする
+ * 練習2
+ * 17-1で作成したコードをtry-catch文を用いて例外処理するよう修正
+ * 例外処理では次の動作を行う
+ * ①「NullPointerException 例外をcatchしました」と表示
+ * ②「--スタックトレース（ここから）--」と表示
+ * ③スタックトレースを表示
+ * ④「--スタックトレース（ここまで）--」と表示
  */
 public class Exercise1 {
     public static void main(String[] args) {
+        try {
             String s = null;
             System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException 例外をcatchしました");
+            System.out.println("--スタックトレース（ここから）--");
+            e.printStackTrace(System.out);
+            System.out.println("--スタックトレース（ここまで）--");
+        }
     }
 }
