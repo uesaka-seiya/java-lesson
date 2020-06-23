b05372d0520e421541ddfc32495648cc839821b4
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-06-23 11:58:32 +0900

    fix: ひし形が描画されるよう修正した
    文字列結合にStringBuilderを使用した
    addAsterisk()をStringクラスのrepeat()で置き換えた
    addBlank()をStringクラスのrepeat()で置き換えた
    メインメソッドを修正してひし形を描画するようにした

--- Algorithm/DiamondPrinter.java
+++ Algorithm/DiamondPrinter.java
@@ -15,62 +15,35 @@ public class DiamondPrinter {
      * @param height 高さ
      * @return 指定の高さのひし形の文字列
      */
-    public String createDiamond(int height) {
-        String printLine = "";
+    public static String createDiamond(int height) {
+        StringBuilder printLine = new StringBuilder();
         int middle = 2 * height - 1;
         int blank = height;
         for (int asterisk = 1; asterisk <= middle; asterisk += 2) {
             blank--;
-            addBlank(blank, printLine);
-            addAsterisk(asterisk, printLine);
-            addBlank(blank, printLine);
-            printLine += "\n";
+            printLine.append("　".repeat(blank));
+            printLine.append("＊".repeat(asterisk));
+            printLine.append("\n");
         }
         for (int asterisk = middle - 2; asterisk >= 1; asterisk -= 2) {
-            addBlank(blank + 1, printLine);
-            addAsterisk(asterisk, printLine);
-            addBlank(blank + 1, printLine);
-            printLine += "\n";
+            printLine.append("　".repeat(blank + 1));
+            printLine.append("＊".repeat(asterisk));
+            printLine.append("\n");
             blank++;
         }
-        return printLine;
-    }
-
-    /**
-     * 文字列変数に指定した数だけアスタリスクを追加するメソッド
-     *
-     * @param asterisk  指定するアスタリスクの数
-     * @param printLine 追加先の文字列変数
-     */
-    public void addAsterisk(int asterisk, String printLine) {
-        for (int i = 1; i <= asterisk; i++) {
-            printLine += "＊";
-        }
-    }
-
-    /**
-     * 文字列変数に指定した数だけスペースを追加するメソッド
-     *
-     * @param blank     指定するスペースの数
-     * @param printLine 追加先の文字列変数
-     */
-    public void addBlank(int blank, String printLine) {
-        for (int i = 1; i <= blank; i++) {
-            printLine += "　";
-        }
+        return printLine.toString();
     }
 
     public static void main(String[] args) {
-        DiamondPrinter diamondPrinter = new DiamondPrinter();
         // 引数の数値が1の場合
-        diamondPrinter.createDiamond(1);
+        System.out.println(createDiamond(1));
         System.out.print("\n");
 
         // 引数の数値が2の場合
-        diamondPrinter.createDiamond(2);
+        System.out.println(createDiamond(2));
         System.out.print("\n");
 
         // 引数の数値が3の場合
-        diamondPrinter.createDiamond(3);
+        System.out.println(createDiamond(3));
     }
 }
